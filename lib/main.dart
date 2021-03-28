694f1378aa8ea6a309008ef655560942c1e4e2d2
IrosTheBeggar <paul.sori@pm.me>
2021-03-27 20:15:43 -0400

    added an about screen

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,8 @@ import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:rxdart/rxdart.dart';
 
+import 'screens/about_screen.dart';
+
 //final _isTtsSupported = kIsWeb || !Platform.isMacOS;
 
 // You might want to provide this using dependency injection rather than a
@@ -37,10 +39,17 @@ Future<void> main() async {
     ),
   );
   runApp(new MaterialApp(
-    title: 'Audio Service Demo',
-    theme: ThemeData(primarySwatch: Colors.blue),
-    home: MStreamApp(),
-  ));
+      title: 'Audio Service Demo',
+      home: MStreamApp(),
+      theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Color(0xFF212121),
+          primaryColorDark: Color(0xFF000000),
+          primaryColorLight: Color(0xFF484848),
+          accentColor: Color(0xFFffab00),
+          buttonColor: Color(0xFFFFAB00),
+          scaffoldBackgroundColor: Color(0xFFe1e2e1),
+          cardColor: Color(0xFFffffff))));
 }
 
 class MStreamApp extends StatefulWidget {
@@ -73,6 +82,44 @@ class _MStreamAppState extends State<MStreamApp>
               ],
               controller: _tabController),
         ),
+        drawer: Drawer(
+            child: ListView(children: <Widget>[
+          ListTile(
+            title: Text(
+              'mStream Music',
+              style: TextStyle(
+                  fontFamily: 'Jura',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28,
+                  color: Color(0xFFffab00)),
+            ),
+            onTap: () {
+              Navigator.of(context).pop();
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => AboutScreen()));
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.folder),
+            title: Text(
+              'File Explorer',
+              style: TextStyle(
+                  fontFamily: 'Jura',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 17),
+            ),
+            onTap: () {
+              // if(serverList.length > 0) {
+              //   getFileList("", wipeBackCache: true);
+              // }else {
+              //   _setupStartScreen();
+              // }
+              Navigator.of(context).pop();
+              _tabController.animateTo(0);
+            },
+          ),
+        ])),
         body: TabBarView(
             children: [TestScreen(), TestScreen()], controller: _tabController),
         bottomNavigationBar: BottomBar());
@@ -908,159 +955,6 @@ class MediaLibrary {
   };
 }
 
-/// This task defines logic for speaking a sequence of numbers using
-/// text-to-speech.
-//class TextPlayerHandler extends BaseAudioHandler with QueueHandler {
-//  final _tts = Tts();
-//  final _sleeper = Sleeper();
-//  Completer _completer;
-//  var _index = 0;
-//  bool _interrupted = false;
-//  var _running = false;
-//
-//  bool get _playing => playbackState.value.playing;
-//
-//  TextPlayerHandler() {
-//    _init();
-//  }
-//
-//  Future<void> _init() async {
-//    final session = await AudioSession.instance;
-//    // Handle audio interruptions.
-//    session.interruptionEventStream.listen((event) {
-//      if (event.begin) {
-//        if (_playing) {
-//          pause();
-//          _interrupted = true;
-//        }
-//      } else {
-//        switch (event.type) {
-//          case AudioInterruptionType.pause:
-//          case AudioInterruptionType.duck:
-//            if (!_playing && _interrupted) {
-//              play();
-//            }
-//            break;
-//          case AudioInterruptionType.unknown:
-//            break;
-//        }
-//        _interrupted = false;
-//      }
-//    });
-//    // Handle unplugged headphones.
-//    session.becomingNoisyEventStream.listen((_) {
-//      if (_playing) pause();
-//    });
-//    queue.add(List.generate(
-//        10,
-//        (i) => MediaItem(
-//              id: 'tts_${i + 1}',
-//              album: 'Numbers',
-//              title: 'Number ${i + 1}',
-//              artist: 'Sample Artist',
-//              extras: {'number': i + 1},
-//              duration: Duration(seconds: 1),
-//            )));
-//  }
-//
-//  Future<void> run() async {
-//    _completer = Completer();
-//    _running = true;
-//    while (_running) {
-//      try {
-//        if (playbackState.value.playing) {
-//          mediaItem.add(queue.value[_index]);
-//          playbackState.add(playbackState.value.copyWith(
-//            updatePosition: Duration.zero,
-//          ));
-//          AudioService.androidForceEnableMediaButtons();
-//          await Future.wait([
-//            _tts.speak('${mediaItem.value.extras["number"]}'),
-//            _sleeper.sleep(Duration(seconds: 1)),
-//          ]);
-//          if (_index + 1 < queue.value.length) {
-//            _index++;
-//          } else {
-//            _running = false;
-//          }
-//        } else {
-//          await _sleeper.sleep();
-//        }
-//      } on SleeperInterruptedException {} on TtsInterruptedException {}
-//    }
-//    _index = 0;
-//    mediaItem.add(queue.value[_index]);
-//    playbackState.add(playbackState.value.copyWith(
-//      updatePosition: Duration.zero,
-//    ));
-//    if (playbackState.value.processingState != AudioProcessingState.idle) {
-//      stop();
-//    }
-//    _completer.complete();
-//    _completer = null;
-//  }
-//
-//  @override
-//  Future<void> skipToQueueItem(int index) async {
-//    _index = index;
-//    _signal();
-//  }
-//
-//  @override
-//  Future<void> play() async {
-//    if (playbackState.value.playing) return;
-//    final session = await AudioSession.instance;
-//    // flutter_tts doesn't activate the session, so we do it here. This
-//    // allows the app to stop other apps from playing audio while we are
-//    // playing audio.
-//    if (await session.setActive(true)) {
-//      // If we successfully activated the session, set the state to playing
-//      // and resume playback.
-//      playbackState.add(playbackState.value.copyWith(
-//        controls: [MediaControl.pause, MediaControl.stop],
-//        processingState: AudioProcessingState.ready,
-//        playing: true,
-//      ));
-//      if (_completer == null) {
-//        run();
-//      } else {
-//        _sleeper.interrupt();
-//      }
-//    }
-//  }
-//
-//  @override
-//  Future<void> pause() async {
-//    _interrupted = false;
-//    playbackState.add(playbackState.value.copyWith(
-//      controls: [MediaControl.play, MediaControl.stop],
-//      processingState: AudioProcessingState.ready,
-//      playing: false,
-//    ));
-//    _signal();
-//  }
-//
-//  @override
-//  Future<void> stop() async {
-//    playbackState.add(playbackState.value.copyWith(
-//      controls: [],
-//      processingState: AudioProcessingState.idle,
-//      playing: false,
-//    ));
-//    _running = false;
-//    _signal();
-//    // Wait for the speech to stop
-//    await _completer?.future;
-//    // Shut down this task
-//    await super.stop();
-//  }
-//
-//  void _signal() {
-//    _sleeper.interrupt();
-//    _tts.interrupt();
-//  }
-//}
-
 /// An object that performs interruptable sleep.
 class Sleeper {
   Completer? _blockingCompleter;
@@ -1090,51 +984,5 @@ class Sleeper {
 }
 
 class SleeperInterruptedException {}
-
-/// A wrapper around FlutterTts that makes it easier to wait for speech to
-/// complete.
-//class Tts {
-//  final FlutterTts _flutterTts = new FlutterTts();
-//  Completer _speechCompleter;
-//  bool _interruptRequested = false;
-//  bool _playing = false;
-//
-//  Tts() {
-//    _flutterTts.setCompletionHandler(() {
-//      _speechCompleter?.complete();
-//    });
-//  }
-//
-//  bool get playing => _playing;
-//
-//  Future<void> speak(String text) async {
-//    _playing = true;
-//    if (!_interruptRequested) {
-//      _speechCompleter = Completer();
-//      await _flutterTts.speak(text);
-//      await _speechCompleter.future;
-//      _speechCompleter = null;
-//    }
-//    _playing = false;
-//    if (_interruptRequested) {
-//      _interruptRequested = false;
-//      throw TtsInterruptedException();
-//    }
-//  }
-//
-//  Future<void> stop() async {
-//    if (_playing) {
-//      await _flutterTts.stop();
-//      _speechCompleter?.complete();
-//    }
-//  }
-//
-//  void interrupt() {
-//    if (_playing) {
-//      _interruptRequested = true;
-//      stop();
-//    }
-//  }
-//}
 
 class TtsInterruptedException {}
---
+++ lib/screens/about_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class AboutScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("About"),
+        ),
+        body: Container(
+            padding: EdgeInsets.all(40.0),
+            child: ListView(children: [
+              Image(image: AssetImage('graphics/mstream-logo.png')),
+              Container(
+                height: 15,
+              ),
+              Text('mStream Mobile v0.5',
+                  style: TextStyle(
+                      fontFamily: 'Jura',
+                      color: Color(0xFF000000),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20)),
+              Text('Beta Edition',
+                  style: TextStyle(
+                      fontFamily: 'Jura',
+                      color: Color(0xFF000000),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20)),
+              Container(
+                height: 45,
+              ),
+              Text('Developed By:',
+                  style: TextStyle(
+                      fontFamily: 'Jura',
+                      color: Color(0xFF000000),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20)),
+              Text('Paul Sori',
+                  style: TextStyle(
+                      fontFamily: 'Jura',
+                      color: Color(0xFF000000),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20)),
+              Text('paul@mstream.io',
+                  style: TextStyle(
+                      fontFamily: 'Jura',
+                      color: Color(0xFF000000),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20)),
+            ])));
+  }
+}
