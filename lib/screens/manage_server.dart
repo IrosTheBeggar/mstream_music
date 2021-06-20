244374a162ccd72fa86b22dc4d3c6c94f99153d5
IrosTheBeggar <paul.sori@pm.me>
2021-06-20 00:18:56 -0400

    cleanup manage server code

--- lib/screens/manage_server.dart
+++ lib/screens/manage_server.dart
@@ -23,60 +23,61 @@ class ManageServersScreen extends StatelessWidget {
         ),
         body: Row(children: [
           Expanded(
-            child: SizedBox(
-              child: StreamBuilder<List<Server>>(
-                  stream: ServerManager().serverListStream,
-                  builder: (context, snapshot) {
-                    final List<Server> cServerList = snapshot.data ?? [];
-                    return ListView.builder(
-                        physics: const AlwaysScrollableScrollPhysics(),
-                        itemCount: cServerList.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return ListTile(
-                            title: Text(cServerList[index].url,
-                                style: TextStyle(
-                                    color: Colors.black, fontSize: 18)),
-                            trailing: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                IconButton(
-                                    icon: Icon(Icons.edit),
-                                    color: Color(0xFF212121),
-                                    tooltip: 'Edit Server',
-                                    onPressed: () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                EditServerScreen(
-                                                    editThisServer: index)),
-                                      );
-                                    }),
-                                IconButton(
-                                  icon: Icon(
-                                    Icons.delete,
-                                    color: Colors.redAccent,
-                                  ),
-                                  tooltip: 'Delete Server',
-                                  onPressed: () {
-                                    showDialog(
-                                      context: context,
-                                      builder: (BuildContext context) {
-                                        // return object of type Dialog
-                                        return DeleteServerDialog(
-                                            cServer: ServerManager()
-                                                .serverList[index]);
+              child: SizedBox(
+                  child: StreamBuilder<List<Server>>(
+                      stream: ServerManager().serverListStream,
+                      builder: (context, snapshot) {
+                        final List<Server> cServerList = snapshot.data ?? [];
+                        return ListView.separated(
+                            physics: const AlwaysScrollableScrollPhysics(),
+                            itemCount: cServerList.length,
+                            separatorBuilder:
+                                (BuildContext context, int index) =>
+                                    Divider(height: 3, color: Colors.black),
+                            itemBuilder: (BuildContext context, int index) {
+                              return ListTile(
+                                title: Text(cServerList[index].url,
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 18)),
+                                trailing: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    IconButton(
+                                        icon: Icon(Icons.edit),
+                                        color: Color(0xFF212121),
+                                        tooltip: 'Edit Server',
+                                        onPressed: () {
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    EditServerScreen(
+                                                        editThisServer: index)),
+                                          );
+                                        }),
+                                    IconButton(
+                                      icon: Icon(
+                                        Icons.delete,
+                                        color: Colors.redAccent,
+                                      ),
+                                      tooltip: 'Delete Server',
+                                      onPressed: () {
+                                        showDialog(
+                                          context: context,
+                                          builder: (BuildContext context) {
+                                            // return object of type Dialog
+                                            return DeleteServerDialog(
+                                                cServer: ServerManager()
+                                                    .serverList[index]);
+                                          },
+                                        );
                                       },
-                                    );
-                                  },
+                                    ),
+                                  ],
                                 ),
-                              ],
-                            ),
-                          );
-                        });
-                  }),
-            ),
-          )
+                              );
+                            });
+                      })))
         ]));
   }
 }
